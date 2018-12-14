c1215c718a254703d95b5f7a5a60c7e0ad066132
Takeshi <iwatakeshi@gmail.com>
2018-12-13 18:43:32 -0700

    Complete Parser

--- metal/frontend/parser/Parser.java
+++ metal/frontend/parser/Parser.java
@@ -1,122 +1,173 @@
 package metal.frontend.parser;
 
-import java.util.List;                                 
+import java.util.List;
 
 import static metal.frontend.scanner.TokenType.*;
+import metal.frontend.scanner.TokenType;
 import metal.frontend.scanner.Token;
 import metal.frontend.parser.grammar.*;
+import metal.Metal;
 
-class Parser {                                         
-  private final List<Token> tokens;                    
-  private int current = 0;                             
 
-  Parser(List<Token> tokens) {                         
-    this.tokens = tokens;                              
-  } 
-  private Expression expression() {
-    return equality();       
+class Parser {
+
+  private static class ParseError extends RuntimeException {}
+
+  private final List<Token> tokens;
+  private int current = 0;
+
+  Parser(List<Token> tokens) {
+    this.tokens = tokens;
   }
-  private Expression equality() {                         
+
+  private Expression expression() {
+    return equality();
+  }
+
+  private Expression equality() {
     Expression expression = comparison();
 
-    while (match('!=', '==')) {        
-      Token operator = previous();                  
-      Expression right = comparison();                    
+    while (match("!=", "==")) {
+      Token operator = previous();
+      Expression right = comparison();
       expression = new Expression.Binary(expression, operator, right);
-    }                                               
+    }
 
-    return expression;                                    
-  } 
+    return expression;
+  }
 
-  private Expression comparison() {                                
+  private Expression comparison() {
     Expression expression = addition();
 
-    while (match('>', '>=', '<', '<=')) {
-      Token operator = previous();                           
-      Expression right = addition();                               
-      expression = new Expression.Binary(expression, operator, right);         
-    }                                                        
+    while (match(">", ">=", "<", "<=")) {
+      Token operator = previous();
+      Expression right = addition();
+      expression = new Expression.Binary(expression, operator, right);
+    }
 
-    return expression;                                             
-  } 
-  
-  private Expression addition() {                         
+    return expression;
+  }
+
+  private Expression addition() {
     Expression expression = multiplication();
 
-    while (match('-', '+')) {                    
-      Token operator = previous();                  
-      Expression right = multiplication();                
+    while (match("-", "+")) {
+      Token operator = previous();
+      Expression right = multiplication();
       expression = new Expression.Binary(expression, operator, right);
-    }                                               
+    }
 
-    return expression;                                    
-  }                                                 
+    return expression;
+  }
 
-  private Expression multiplication() {                   
-    Expression expression = unary();                            
+  private Expression multiplication() {
+    Expression expression = unary();
 
-    while (match('/', '*')) {                    
-      Token operator = previous();                  
-      Expression right = unary();                         
+    while (match("/", "*")) {
+      Token operator = previous();
+      Expression right = unary();
       expression = new Expression.Binary(expression, operator, right);
-    }                                               
+    }
 
-    return expression;                                    
-  } 
+    return expression;
+  }
 
-  private Expression unary() {                     
-    if (match('!', '-')) {                
-      Token operator = previous();           
-      Expression right = unary();                  
+  private Expression unary() {
+    if (match("!", "-")) {
+      Token operator = previous();
+      Expression right = unary();
       return new Expression.Unary(operator, right);
     }
 
-    return primary();                        
-  } 
-
-  private Expression primary() {                                 
-    if (match('FALSE')) return new Expression.Literal(false);      
-    if (match('TRUE')) return new Expression.Literal(true);        
-    if (match('NIL')) return new Expression.Literal(null);
-
-    if (match(NumberLiteral, StringLiteral)) {                           
-      return new Expression.Literal(previous().literal);         
-    }                                                      
-
-    if (match('(') {                               
-      Expression expression = expression();                            
-      consume(')', "Expect ')' after expression.");
-      return new Expression.Group(expression);                      
-    }                                                      
-  } 
-
-  private boolean match(TokenType... types) {
-    for (TokenType type : types) {           
-      if (check(type)) {                     
-        advance();                           
-        return true;                         
-      }                                      
-    }
-
-    return false;                            
-  } 
-  private boolean check(TokenType type) {
-    if (isAtEnd()) return false;         
-    return peek().type == type;          
-  } 
-  private Token advance() {   
-    if (!isAtEnd()) current++;
-    return previous();        
-  } 
-  private boolean isAtEnd() {      
-    return peek().type == EOF;     
+    return primary();
   }
 
-  private Token peek() {           
-    return tokens.get(current);    
-  }                                
+  private Expression primary() {
+    if (match(BooleanLiteral))
+      return new Expression.Literal(Boolean.parseBoolean(previous().lexeme));
+    if (match(NullLiteral))
+      return new Expression.Literal(null);
 
-  private Token previous() {       
+    if (match(NumberLiteral, StringLiteral)) {
+      return new Expression.Literal(previous().literal);
+    }
+
+    if (match("(")) {
+      Expression expression = expression();
+      consume(")", "Expect ')' after expression.");
+      return new Expression.Group(expression);
+    }
+
+    throw error(peek(), "Expect expression."); 
+  }
+
+  private boolean match(TokenType... types) {
+    for (TokenType type : types) {
+      if (check(type)) {
+        advance();
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private boolean match(String... types) {
+    for (String type : types) {
+      if (check(type)) {
+        advance();
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private Token consume(TokenType type, String message) {
+    if (check(type))
+      return advance();
+
+    throw error(peek(), message);
+  }
+
+  private Token consume(String type, String message) {
+    if(check(type)) {
+      return advance();
+    }
+    throw error(peek(), message);
+  }
+
+  private boolean check(TokenType type) {
+    if (isAtEnd())
+      return false;
+    return peek().type == type;
+  }
+
+  private boolean check(String type) {
+    if (isAtEnd())
+      return false;
+    return peek().type == Token.getTokenType(type);
+  }
+
+  private Token advance() {
+    if (!isAtEnd())
+      current++;
+    return previous();
+  }
+
+  private boolean isAtEnd() {
+    return peek().type == EOF;
+  }
+
+  private Token peek() {
+    return tokens.get(current);
+  }
+
+  private Token previous() {
     return tokens.get(current - 1);
-  }                                                   
-} 
+  }
+
+  private ParseError error(Token token, String message) {
+    Metal.error(token, message);                           
+    return new ParseError();                             
+  }
+}
--- metal/frontend/scanner/Token.java
+++ metal/frontend/scanner/Token.java
@@ -1,27 +1,131 @@
 package metal.frontend.scanner;
 
+import java.text.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import static metal.frontend.scanner.TokenType.*;
 import metal.frontend.scanner.Token;
 
-public class Token {                                                     
+public class Token {
   public final TokenType type;
-  public final ArrayList<TokenType> types;                                           
-  public final String lexeme;                                           
-  public final Object literal;                                           
-  public final int line; 
+  public final String lexeme;
+  public final Object literal;
+  public final int line;
   public final int column;
 
-  public Token(TokenType type, String lexeme, Object literal, int line, int column) {
-    this.type = type;                                             
-    this.lexeme = lexeme;                                         
-    this.literal = literal;                                       
-    this.line = line;
-    this.column = column;
-    this.types = new ArrayList<>();                                            
+  private static final Map<String, TokenType> reserved;
+  private static final Map<String, TokenType> operators;
+  private static final Map<String, TokenType> punctuations;
+
+  static {
+    reserved = new HashMap<>();
+    reserved.put("and", Reserved);
+    reserved.put("class", Reserved);
+    reserved.put("else", Reserved);
+    reserved.put("false", Reserved);
+    reserved.put("for", Reserved);
+    reserved.put("fun", Reserved);
+    reserved.put("if", Reserved);
+    reserved.put("nil", Reserved);
+    reserved.put("or", Reserved);
+    reserved.put("print", Reserved);
+    reserved.put("return", Reserved);
+    reserved.put("super", Reserved);
+    reserved.put("this", Reserved);
+    reserved.put("true", Reserved);
+    reserved.put("var", Reserved);
+    reserved.put("while", Reserved);
+
+    operators = new HashMap<>();
+    operators.put("+", Operator);
+    operators.put("-", Operator);
+    operators.put("*", Operator);
+    operators.put("/", Operator);
+    operators.put("!", Operator);
+    operators.put("=", Operator);
+    operators.put("<", Operator);
+    operators.put(">", Operator);
+    operators.put("+=", Operator);
+    operators.put("-=", Operator);
+    operators.put("*=", Operator);
+    operators.put("/=", Operator);
+    operators.put("!=", Operator);
+    operators.put("==", Operator);
+    operators.put("<=", Operator);
+    operators.put(">=", Operator);
+    operators.put(".", Operator);
+
+    punctuations = new HashMap<>();
+    punctuations.put("(", Punctuation);
+    punctuations.put(")", Punctuation);
+    punctuations.put("{", Punctuation);
+    punctuations.put("}", Punctuation);
+    punctuations.put(",", Punctuation);
+    punctuations.put(";", Punctuation);
   }
 
-  public String toString() {                                      
-    return "Type: " + type + ", Lexeme: " + lexeme + ", Value: " + literal + ", Line: " + line + ", Column: " + column;                   
+  public Token(TokenType type, String lexeme, Object literal, int line, int column) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+    this.column = column;
   }
-}                                                                 
+
+  public String toString() {
+    return "Type: " + type + ", Lexeme: " + lexeme + ", Value: " + literal + ", Line: " + line + ", Column: " + column;
+  }
+
+  public static Boolean isReserved(String lexeme) {
+    return reserved.get(lexeme) != null;
+  }
+
+  public static Boolean isOperator(String lexeme) {
+    return operators.get(lexeme) != null;
+  }
+
+  public static Boolean isPunctuation(String lexeme) {
+    return punctuations.get(lexeme) != null;
+  }
+
+  public static Boolean isBooleanLiteral(String lexeme) {
+    return lexeme == "true" || lexeme == "false";
+  }
+
+  public static Boolean isNullLiteral(String lexeme) {
+    return lexeme == "null";
+  }
+
+  public static Boolean isNumberLiteral(String lexeme) {
+    DecimalFormat decimalFormat = new DecimalFormat("#");
+    try {
+      decimalFormat.parse(lexeme).doubleValue();
+      return true;
+   } catch (ParseException e) {
+      return false;
+   }
+  }
+
+  public static Boolean isStringLiteral(String literal) {
+    return (literal.charAt(0) == '"' && literal.charAt(literal.length() - 1) == '"') ||
+    (literal.charAt(0) == '\'' && literal.charAt(literal.length() - 1) == '\'');
+  }
+
+  public static TokenType getTokenType(String lexeme) {
+    if (isReserved(lexeme)) {
+      return TokenType.Reserved;
+    }
+
+    if (isOperator(lexeme)) {
+      return TokenType.Operator;
+    }
+
+    if(isPunctuation(lexeme)) {
+      return TokenType.Punctuation;
+    }
+
+    return TokenType.OtherType;
+  }
+}
